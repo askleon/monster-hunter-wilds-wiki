91fa4aee0a2b6dd0cffa6d4924b896f1327ec3b4
Leon Ask <leon.ask@outlook.com>
2024-09-30 01:12:20 +0200

    deleted

--- app/loadout/[name]/page.tsx
+++
@@ -1,5 +0,0 @@
-import LoadoutBuilder from '@/app/components/LoadoutBuilder';
-
-export default function EditLoadoutPage({ params }: { params: { name: string } }) {
-  return <LoadoutBuilder loadoutName={decodeURIComponent(params.name)} />;
-}
